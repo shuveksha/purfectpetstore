a6b237d6b11d5a84fe20a5bd9810491934998346
Tulsi Gautam <tulsigautam05@gmail.com>
2022-09-25 18:44:27 +0545

    Update app.js

--- app.js
+++ app.js
@@ -3,32 +3,29 @@ const chalk = require('chalk');
 const debug = require('debug')('petshopstore');
 const morgan = require('morgan');
 const path = require('path');
-const bodyparser= require('body-parser');
-const mysql= require('mysql');
-const dotenv= require('dotenv');
-const cookieParser= require('cookie-parser');
-
-dotenv.config({path:'./.env'});
-
+const bodyparser = require('body-parser');
+const mysql = require('mysql');
+require('dotenv').config();
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const passport = require('passport');
+const flash = require('connect-flash');
 const app = express(); //object instantiated
 app.set('view engine', 'ejs'); //registering ejs (view engine)
 
-
 //establishing(creating) db connection
- const db=mysql.createConnection(
-  {
-  host:process.env.DATABASE_HOST,
-  user:process.env.DATABASE_USER,
-  password:process.env.DATABASE_PASSWORD,
-  database:process.env.DATABASE
-}
-)
-db.connect((error)=>{
+const db = mysql.createConnection({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE
+})
+db.connect((error) => {
 
-  if(error){
-    debug('error in connecting database');
-  }
-  else{
+  if (error) {
+    console.log(error);
+  } else {
     debug('database connected succesfully')
   }
 })
@@ -36,13 +33,32 @@ db.connect((error)=>{
 
 app.use(morgan('tiny')); // middleware
 app.use(express.static(path.join(__dirname, './public')));
-app.use('/css',express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
-
-app.use('/js',express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
-
-app.use(express.urlencoded({extended:false}));
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(express.json());
 app.use(cookieParser()); //cookies haru use garna
+
+
+
+
+
+
+// required for passport
+app.use(session({
+  name:'mycookie',
+  secret: 'oursecret',
+  resave: false,
+  saveUninitialized: false
+})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+
+
 
 //homepage ko chuttai route banauna baki cha
 
@@ -51,7 +67,9 @@ app.get('/', (req, res) => {
 
 })
 
+require('./controllers/passport')(passport); // pass passport for configuration
 //****importing routes***
+require('./routes/adminroutes.js')(app, passport);
 
 const catrouter = require('./routes/catroutes');
 app.use('/catproducts', catrouter);
@@ -72,42 +90,9 @@ app.use('/signup',signuprouter);
 
 
 
-// admin routes start//
 
-app.get('/orderdashboard',(req,res)=>{   //eni haruko chuttai routes chai banai sakeko chaina aile lai pachi banaune routes folder ma banayera
-    res.render('adminorders_dashboard');
-})
-
-app.get('/orderdetails',(req,res)=>{
-  res.render('adminorder_details');
-})
-
-app.get('/catview',(req,res)=>{
-  res.render('admincat_view');
-})
-
-  app.get('/addpage',(req,res)=>{
-    res.render('adminadd_page');
-  })
-
-  app.get('/updatepage',(req,res)=>{
-    res.render('adminupdate_page');
-  })
-
-    app.get('/fishview',(req,res)=>{
-      res.render('adminfish_view');
-})
-
-app.get('/productdetail',(req,res)=>{
-  res.render('adminproduct_details');
-})
-
-app.get('/dogview',(req,res)=>{
-  res.render('admindogs_view');
-})
 
 
 app.listen(3000, () => {
   debug(`listening on port ${chalk.blue.bold('3000')}`);
 });
-
